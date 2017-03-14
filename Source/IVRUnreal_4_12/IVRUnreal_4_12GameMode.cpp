4339360150834b8d581eab8393a37f07ddbdbf50
xyhak47 <474318838@qq.com>
2017-03-14 10:00:08 +0800

    create project

---
+++ Source/IVRUnreal_4_12/IVRUnreal_4_12GameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "IVRUnreal_4_12.h"
+#include "IVRUnreal_4_12GameMode.h"
+
+
+
+
---
+++ Source/IVRUnreal_4_12/IVRUnreal_4_12GameMode.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "IVRUnreal_4_12GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class IVRUNREAL_4_12_API AIVRUnreal_4_12GameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
